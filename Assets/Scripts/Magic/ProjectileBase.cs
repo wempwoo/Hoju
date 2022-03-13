c490ad2c65928c591abab1b6094324cea731e8f3
wempwoo <wemp.woo@gmail.com>
2022-03-13 13:13:07 +0900

    Projectileの当たり判定

--- Assets/Scripts/Magic/ProjectileBase.cs
+++ Assets/Scripts/Magic/ProjectileBase.cs
@@ -29,4 +29,12 @@ public class ProjectileBase : Entity
             Destroy();
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            this.Destroy();
+        }
+    }
 }
