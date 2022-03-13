654c59691c8c5fb7d4db5daaeb9d12953fd4a3c9
wempwoo <wemp.woo@gmail.com>
2022-03-13 20:49:57 +0900

    Projectileの分散

--- Assets/Scripts/Commons/ArcDegree.cs
+++ Assets/Scripts/Commons/ArcDegree.cs
@@ -13,6 +13,16 @@ public struct ArcDegree
         this.value = arcDegree;
     }
 
+    public static ArcDegree operator +(ArcDegree a, ArcDegree b)
+    {
+        return new ArcDegree(a.value + b.value);
+    }
+
+    public static ArcDegree operator -(ArcDegree a, ArcDegree b)
+    {
+        return new ArcDegree(a.value - b.value);
+    }
+
     public static ArcDegree Of(Vector2 v)
     {
         return new ArcDegree(Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg);
@@ -44,6 +54,21 @@ public struct ArcDegree
     public static float Sin(float Deg)
     {
         return Mathf.Sin(Mathf.Deg2Rad * Deg);
+    }
+
+    /// <summary>
+    /// vectorを回転させる
+    /// </summary>
+    /// <param name="vector"></param>
+    /// <param name="rotation"></param>
+    /// <returns></returns>
+    public static Vector2 Rotate(Vector2 vector, ArcDegree rotation)
+    {
+        var distance = Vector2.Distance(Vector2.zero, vector);
+        var currentDirection = Of(vector);
+        var rotated = currentDirection + rotation;
+
+        return ToVector(rotated, distance);
     }
 }
 
--- Assets/Scripts/Magic/ProjectileBase.cs
+++ Assets/Scripts/Magic/ProjectileBase.cs
@@ -6,11 +6,16 @@ public class ProjectileBase : Entity
 {
     public Seconds Lifespan { get; set; } = new Seconds(3);
 
+    public ArcDegree Dispersion { get; set; } = new ArcDegree(10);
+
     private Seconds elapsed = new Seconds(0);
 
     // Start is called before the first frame update
     void Start()
     {
+        float dispersionHalf = Dispersion.value / 2;
+        ArcDegree directionRev = new ArcDegree(Random.Range(-dispersionHalf, dispersionHalf));
+        this.Velocity = ArcDegree.Rotate(this.Velocity, directionRev);
     }
 
     // Update is called once per frame
