8a6ad7bfa6d2850b6640dbd09d779abfd9ee0c1c
wempwoo <wemp.woo@gmail.com>
2022-03-13 20:51:30 +0900

    コメント

--- Assets/Scripts/Magic/ProjectileBase.cs
+++ Assets/Scripts/Magic/ProjectileBase.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 魔法による投射物
+/// </summary>
 public class ProjectileBase : Entity
 {
+    /// <summary>
+    /// 投射物の寿命
+    /// </summary>
     public Seconds Lifespan { get; set; } = new Seconds(3);
 
+    /// <summary>
+    /// 投射角度の分散（例えば10度なら±5度）
+    /// </summary>
     public ArcDegree Dispersion { get; set; } = new ArcDegree(10);
 
     private Seconds elapsed = new Seconds(0);
