2b2390502c5b359f8e4f873d02fcf6ff8a19c0f1
wempwoo <wemp.woo@gmail.com>
2022-03-17 21:05:02 +0900

    コミット漏れ

--- Assets/Scripts/BattleScene/Player.cs
+++ Assets/Scripts/BattleScene/Player.cs
@@ -13,23 +13,6 @@ namespace BattleScene
         {
             this.manaroid = Prefabs.Instantiate<Manaroid>("ManaroidPrefab");
             this.manaroid.Position = this.Position + new Vector2(0, 1);
-            this.manaroid.circuit = new MagicCircuit(this.CreateSlotsTree());
-        }
-
-        private SlotNode CreateSlotsTree()
-        {
-            var slot1 = new SlotNode();
-            slot1.spell = new SpellOrb();
-
-            var slot2 = new SlotNode();
-            slot2.spell = new SpellOrb();
-            slot1.nextSlot = slot2;
-
-            var slot3 = new SlotNode();
-            slot3.spell = new SpellOrb();
-            slot2.nextSlot = slot3;
-
-            return slot1;
         }
 
         void Update()
