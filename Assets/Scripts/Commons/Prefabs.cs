29ea7be5d8e22e56b9a9f249dbbf9870f1ca781b
wempwoo <wemp.woo@gmail.com>
2022-03-19 15:09:08 +0900

    Prefab not foundのエラー

--- Assets/Scripts/Commons/Prefabs.cs
+++ Assets/Scripts/Commons/Prefabs.cs
@@ -4,7 +4,15 @@ public static class Prefabs
 {
     public static GameObject Load(string name)
     {
-        return Resources.Load("Prefabs/" + name) as GameObject;
+        string path = "Prefabs/" + name;
+        var prefab = Resources.Load(path) as GameObject;
+
+        if (prefab == null)
+        {
+            throw new Exception("not found: " + path);
+        }
+
+        return prefab;
     }
 
     public static T Instantiate<T>(string name)
