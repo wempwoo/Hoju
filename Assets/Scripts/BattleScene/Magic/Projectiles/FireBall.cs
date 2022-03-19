8c83f4e5e056bd69282c766931221c32d42e1eb9
wempwoo <wemp.woo@gmail.com>
2022-03-19 14:22:33 +0900

    着弾時に爆発するFireball

--- Assets/Scripts/BattleScene/Magic/Projectiles/FireBall.cs
+++ Assets/Scripts/BattleScene/Magic/Projectiles/FireBall.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Linq;
+using UnityEngine;
 
 namespace BattleScene
 {
     public class FireBall : ProjectileBehavior
     {
-        public override Seconds Lifespan => throw new NotImplementedException();
+        public override Seconds Lifespan => new Seconds(3);
 
-        public override ArcDegree Dispersion => throw new NotImplementedException();
+        public override ArcDegree Dispersion => new ArcDegree(3);
+
+        protected override void SetupExtends(DriveContext context, Projectile projectile)
+        {
+            var sprite = projectile.GetComponent<SpriteRenderer>();
+            sprite.color = new Color(1.0f, 0.4f, 0.2f);
+        }
 
         public override void OnHit(Projectile projectile, Enemy enemy)
         {
-            throw new NotImplementedException();
+            float range = 2.0f;
+
+            var targetEnemies = Enemy.GetEnemyEntities()
+                .Where(e => Math.Abs(Vector2.Distance(e.Position, projectile.Position)) < range);
+
+            foreach (var target in targetEnemies)
+            {
+                target.Damaged(30);
+            }
         }
     }
 
--- Assets/Scripts/BattleScene/Manaroid.cs
+++ Assets/Scripts/BattleScene/Manaroid.cs
@@ -19,14 +19,14 @@ namespace BattleScene
         private SlotNode CreateSlotsTree()
         {
             var slot1 = new SlotNode();
-            slot1.spell = new ManaBoltOrb();
+            slot1.spell = new FireBallOrb();
 
             var slot2 = new SlotNode();
             slot2.spell = new ManaBoltOrb();
             slot1.nextSlot = slot2;
 
             var slot3 = new SlotNode();
-            slot3.spell = new ManaBoltOrb();
+            slot3.spell = new FireBallOrb();
             slot2.nextSlot = slot3;
 
             return slot1;
