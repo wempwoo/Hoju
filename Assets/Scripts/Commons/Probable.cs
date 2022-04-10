316c436501871834c060622be52c4c57b5308a4e
wempwoo <wemp.woo@gmail.com>
2022-04-10 20:33:07 +0900

    RouteGenerator一旦作ったのでコミット

---
+++ Assets/Scripts/Commons/Probable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 確率による分岐を記述する
+/// </summary>
+public class Probable
+{
+    public static void Single(float probability, Action action)
+    {
+        if (UnityEngine.Random.value < probability)
+        {
+            action();
+        }
+    }
+
+    /// <summary>
+    /// 複数のCaseそれぞれに確率判定がされ、高々一つだけ実行される
+    /// </summary>
+    /// <returns></returns>
+    public static AtMostOneProbable AtMostOne()
+    {
+        return AtMostOneProbable.Start();
+    }
+
+
+    /// <summary>
+    /// 複数のCaseそれぞれに確率判定がされ、高々一つだけ実行される
+    /// </summary>
+    public class AtMostOneProbable
+    {
+        public readonly bool alive;
+
+        private AtMostOneProbable(bool alive)
+        {
+            this.alive = alive;
+        }
+
+        private static readonly AtMostOneProbable Alive = new AtMostOneProbable(true);
+        private static readonly AtMostOneProbable Dead = new AtMostOneProbable(false);
+
+
+        public static AtMostOneProbable Start()
+        {
+            return Alive;
+        }
+
+        public AtMostOneProbable Case(float probability, Action action)
+        {
+            if (!this.alive) return Dead;
+
+            if (UnityEngine.Random.value < probability)
+            {
+                action();
+                return Dead;
+            }
+
+            return Alive;
+        }
+
+        public void Else(Action action)
+        {
+            if (this.alive) action();
+        }
+    }
+
+    /// <summary>
+    /// elementsの要素すべてに対して順次、等確率で実行判断していく
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="probability"></param>
+    /// <param name="elements"></param>
+    /// <param name="action"></param>
+    /// <returns></returns>
+    public static SequenceProbable Sequence<T>(float probability, IEnumerable<T> elements, Action<T> action)
+    {
+        foreach (var e in elements)
+        {
+            if (UnityEngine.Random.value < probability)
+            {
+                action(e);
+                return SequenceProbable.Dead;
+            }
+        }
+
+        return SequenceProbable.Alive;
+    }
+
+    public class SequenceProbable
+    {
+        private readonly bool alive;
+        private SequenceProbable(bool alive) { this.alive = alive; }
+
+        public static readonly SequenceProbable Alive = new SequenceProbable(true);
+        public static readonly SequenceProbable Dead = new SequenceProbable(false);
+
+        public void Else(Action action)
+        {
+            if (this.alive) action();
+        }
+    }
+}
---
+++ Assets/Scripts/ExploreScene/Route/RouteGenerator.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ExploreScene
+{
+    public static class RouteConsts
+    {
+        /// <summary>
+        /// 一度に生成しておくフェーズ数
+        /// </summary>
+        public const int MaxKeepPhases = 5;
+
+        /// <summary>
+        /// 休息部屋の配置間隔
+        /// この数字の倍数フェーズで休息部屋が１つだけ配置されたフェーズを生成する
+        /// </summary>
+        public const int IntervalRestPhase = 4;
+
+        /// <summary>
+        /// 部屋が配置される位置、縦のラインの数
+        /// </summary>
+        public const int Lines = 3;
+    }
+
+    public class RouteGenerator
+    {
+        private ExplorePhase currentPhase;
+        private int currentLine;
+
+        public RouteGenerator()
+        {
+            var firstPhase = new ExplorePhase(1);
+            this.currentPhase = firstPhase;
+            this.currentLine = firstPhase.rooms.First().line;
+
+            var addingPhase = this.currentPhase;
+            for (int i = addingPhase.phase + 1; i <= RouteConsts.MaxKeepPhases; i++)
+            {
+                addingPhase = addingPhase.AddNext();
+            }
+        }
+
+        public void GoTo(int nextLine)
+        {
+            this.currentPhase = this.currentPhase.next;
+            this.currentLine = nextLine;
+            this.currentPhase.GetLastPhase().AddNext();
+        }
+    }
+
+    /// <summary>
+    /// フェーズ
+    /// </summary>
+    public class ExplorePhase
+    {
+        public readonly int phase;
+
+        public readonly List<ExploreRoom> rooms;
+
+        public ExplorePhase next;
+
+        public ExplorePhase(int phase)
+        {
+            this.phase = phase;
+            this.rooms = CreateRooms(phase).ToList();
+        }
+
+        public bool IsSingleRoom { get => this.rooms.Count() == 1; }
+
+        /// <summary>
+        /// 部屋を作成する
+        /// </summary>
+        /// <param name="phase"></param>
+        /// <returns></returns>
+        private static IEnumerable<ExploreRoom> CreateRooms(int phase)
+        {
+            if ((phase - 1) % RouteConsts.IntervalRestPhase == 0)
+            {
+                return new[] { new ExploreRoom(phase, 1) };
+            }
+
+            int roomsCount = DecideRoomsCount();
+
+            switch (roomsCount)
+            {
+                case 1:
+                    return new[] { new ExploreRoom(phase, 2) };
+                case 2:
+                    float rand = UnityEngine.Random.value;
+                    if (rand < 0.4f) return new[] { new ExploreRoom(phase, 1), new ExploreRoom(phase, 2) };
+                    if (rand < 0.8f) return new[] { new ExploreRoom(phase, 2), new ExploreRoom(phase, 3) };
+                    return new[] { new ExploreRoom(phase, 1), new ExploreRoom(phase, 3) };
+                case 3:
+                    return new[] { new ExploreRoom(phase, 1), new ExploreRoom(phase, 2), new ExploreRoom(phase, 3) };
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static int DecideRoomsCount()
+        {
+            float rand = UnityEngine.Random.value;
+
+            if (rand < 0.2f) return 1;
+            if (rand < 0.7f) return 2;
+            return 3;
+        }
+
+        public ExplorePhase AddNext()
+        {
+            var nextPhase = new ExplorePhase(this.phase + 1);
+            this.ConnectToNext(nextPhase);
+
+            return nextPhase;
+        }
+
+        private void ConnectToNext(ExplorePhase next)
+        {
+            this.next = next;
+
+            if (this.IsSingleRoom)
+            {
+                this.rooms.First().ConnectTo(next.rooms);
+                return;
+            }
+
+            if (next.IsSingleRoom)
+            {
+                foreach (var r in this.rooms) r.ConnectTo(next.rooms.First());
+                return;
+            }
+
+            // 両端は確定
+            this.rooms.First().ConnectTo(next.rooms.First());
+            this.rooms.Last().ConnectTo(next.rooms.Last());
+
+            // 1-2, 2-3
+            foreach (var pair in new [] { new [] { 1, 2 }, new[] { 2, 3 } })
+            {
+                int left = pair[0];
+                int right = pair[1];
+                if (this.HasLines(left, right) && next.HasLines(left, right))
+                {
+                    Probable.AtMostOne()
+                        .Case(0.4f, () => this.rooms[left - 1].ConnectTo(right))
+                        .Case(0.4f, () => this.rooms[right - 1].ConnectTo(left));
+                }
+            }
+
+            // 中央の部屋の接続チェック
+            if (this.RoomAt(2).IsNoConnect || next.RoomAt(2).IsNoConnect)
+            {
+                this.RoomAt(2).ConnectTo(2);
+            }
+            else
+            {
+                Probable.Single(0.5f, () => this.RoomAt(2).ConnectTo(2));
+            }
+        }
+
+        /// <summary>
+        /// line1とline2の両方に部屋があればtrue
+        /// </summary>
+        /// <param name="line1"></param>
+        /// <param name="line2"></param>
+        /// <returns></returns>
+        private bool HasLines(int line1, int line2)
+        {
+            return this.rooms.Any(r => r.line == line1)
+                && this.rooms.Any(r => r.line == line2);
+        }
+
+        private ExploreRoom RoomAt(int line)
+        {
+            return this.rooms.Where(r => r.line == line).First();
+        }
+
+        public ExplorePhase GetLastPhase()
+        {
+            if (this.next == null) return this;
+            return this.next.GetLastPhase();
+        }
+    }
+
+    /// <summary>
+    /// 部屋
+    /// </summary>
+    public class ExploreRoom
+    {
+        public readonly int phase;
+        public readonly int line;
+        public readonly List<int> nextLines;
+
+        public ExploreRoom(int phase, int line)
+        {
+            this.phase = phase;
+            this.line = line;
+            this.nextLines = new List<int>();
+        }
+
+        public bool IsNoConnect { get => this.nextLines.Count() == 0; }
+
+        public bool IsConnectable(int fromLine)
+        {
+            return Math.Abs(this.line - fromLine) <= 1;
+        }
+
+        public void ConnectTo(IEnumerable<ExploreRoom> nextRooms)
+        {
+            foreach (var nr in nextRooms) this.ConnectTo(nr);
+        }
+
+        public void ConnectTo(ExploreRoom nextRoom)
+        {
+            this.ConnectTo(nextRoom.line);
+        }
+
+        public void ConnectTo(int line)
+        {
+            this.nextLines.Add(line);
+        }
+    }
+}
